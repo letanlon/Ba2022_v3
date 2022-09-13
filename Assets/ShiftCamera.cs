72567eec727a4b82021ac2a9ddcd1e28142222c6
letanlon <68448738+letanlon@users.noreply.github.com>
2022-09-13 19:44:57 +0200

    testscene 2+cali working together
    camerashift = y-1,61 if on hpreverb

--- Assets/ShiftCamera.cs
+++ Assets/ShiftCamera.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class ShiftCamera : MonoBehaviour
 {
     public bool onHoloLens;
-    [SerializeField] bool isHPReverb = default;
+    [SerializeField] bool isHPReverb;
 
 
     // Start is called before the first frame update
     // Shift camera offset because hp reverb starts in a different position compared to hololens
     void Awake()
     {
-        onHoloLens = SystemInfo.deviceModel.Contains("HoloLens");
+        if(SystemInfo.deviceModel.Contains("HoloLens"))
+        {
+            isHPReverb=false;
+        }
+
         if (isHPReverb)
         {
-            Vector3 position = new Vector3(0f, -1.5f, 0f); // 3f is actually too high, just for testing purposes...
+            Vector3 position = new Vector3(0f, -1.61f, 0f); // 3f is actually too high, just for testing purposes...
 
             this.transform.position = position;
 
